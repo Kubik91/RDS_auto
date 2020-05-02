fd08d8f64f38c17bb95bd3511497c388312a5a84
Павел <shahidkubik@gmail.com>
2020-05-02 21:05:49 +0300

    Version 1

--- main.py
+++ main.py
@@ -580,6 +580,7 @@ def main(all_=True, new=True, train=True):
 
 
 if __name__ == '__main__':
+    print('------------------------------------------------')
     main(train=False, new=False)
     # get_test()
     # get_test_item()
